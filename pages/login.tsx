6301fe9da15d9cef3af6931a482ff9b7a92b5411
Grace Wang <gracewang92@gmail.com>
2022-02-24 17:18:41 +0100

    login w google

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import GoogleLogin from "react-google-login";
 import Web3 from "web3";
 import Web3Modal from "web3modal";
 import { signatureInput } from "./api/auth";
@@ -61,18 +62,36 @@ const GiveTo: NextPage = () => {
     router.push("/give/to")
   }
 
+  function onGoogleSuccess(res: any) {
+    console.log("success", res);
+    router.push("/give/to")
+  }
+
+  function onGoogleFailure(res: any) {
+    console.log("failure", res);
+  }
+
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
       <div className="w-96 flex flex-col">
         <h1 className="text-2xl font-bold mt-5 mb-3">
           Log In
         </h1>
-        <button
-          type="button"
-          className="px-5 py-2.5 mt-3 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
-          Log In with Google
-        </button>
+        <GoogleLogin
+          clientId="314131181818-4oos4568l2idp0t71u5lembd9qb55f9e.apps.googleusercontent.com"
+          onSuccess={onGoogleSuccess}
+          onFailure={onGoogleFailure}
+          render={renderProps => (
+            <button
+              type="button"
+              className="px-5 py-2.5 mt-3 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              onClick={renderProps.onClick} 
+              disabled={renderProps.disabled}
+            >
+              Log In with Google
+            </button>
+          )}
+        />,
         <div className="text-center text-sm uppercase">
           or
         </div>
