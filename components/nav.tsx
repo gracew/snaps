5f7c74efe20ab2cf0fb7ead9a0e4d5a579a91055
Grace Wang <gracewang92@gmail.com>
2022-02-25 17:11:46 +0100

    slightly better logo, set favicon

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,12 +1,18 @@
+import Image from "next/image";
 import AuthButton from "./authButton";
 import PrimaryButton from "./primaryButton";
 
 const Nav = () => {
   return (
-    <nav className="bg-white px-2 py-2.5 dark:bg-gray-800">
+    <nav className="bg-white py-2.5 dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <a href="#" className="flex">
-          <svg className="mr-3 h-10" viewBox="0 0 52 72" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.87695 53H28.7791C41.5357 53 51.877 42.7025 51.877 30H24.9748C12.2182 30 1.87695 40.2975 1.87695 53Z" fill="#76A9FA"></path><path d="M0.000409561 32.1646L0.000409561 66.4111C12.8618 66.4111 23.2881 55.9849 23.2881 43.1235L23.2881 8.87689C10.9966 8.98066 1.39567 19.5573 0.000409561 32.1646Z" fill="#A4CAFE"></path><path d="M50.877 5H23.9748C11.2182 5 0.876953 15.2975 0.876953 28H27.7791C40.5357 28 50.877 17.7025 50.877 5Z" fill="#1C64F2"></path></svg>
+          <Image
+            src="/snaps_100.png"
+            height={50}
+            width={50}
+            alt="Snaps logo"
+          ></Image>
           <span className="sm:hidden self-center text-lg font-semibold whitespace-nowrap dark:text-white">Snaps</span>
         </a>
         <div className="flex w-auto">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <Head>
+      <title>Snaps</title>
+      <meta name="description" content="Show appreciation for teammates and colleagues through digital collectibles." />
+      <link rel="icon" href="/snaps_100.png" />
+    </Head>
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp;
