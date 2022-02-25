245167be9a8a810d51e2fe7cbfa72b105e5992f4
Grace Wang <gracewang92@gmail.com>
2022-02-25 23:29:34 +0100

    fix build

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import AuthButton from "./authButton";
 import PrimaryButton from "./primaryButton";
@@ -8,15 +9,14 @@ const Nav = () => {
   return (
     <nav className="bg-white mt-4 py-2.5 dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="/snaps" className="flex">
+        <Link href="/snaps">
           <Image
             src="/snaps_100.png"
             height={50}
             width={50}
             alt="Snaps logo"
           ></Image>
-          <span className="sm:hidden self-center text-lg font-semibold whitespace-nowrap dark:text-white">Snaps</span>
-        </a>
+        </Link>
         <div className="flex w-auto">
           <PrimaryButton
             text="Give Snaps"
