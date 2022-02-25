9dde81c9137c2b50dbcbf549cbe6945809cad01c
Grace Wang <gracewang92@gmail.com>
2022-02-25 19:20:27 +0100

    save category

--- pages/give/[id]/category.tsx
+++ pages/give/[id]/category.tsx
@@ -6,24 +6,30 @@ import Card from '../../../components/card'
 import Nav from '../../../components/nav'
 import PrimaryButton from '../../../components/primaryButton'
 import SecondaryButton from '../../../components/secondaryButton'
+import { definitions } from "../../../types/supabase"
+import { supabase } from '../../api/supabase'
 
-const types = [
+export const spcTypes = [
   {
+    id: "spc_nurture",
     label: 'Nurture Yourself',
     description: "The best work is built on a foundation of self-care. Don't be afraid to get your needs met.",
     image: "/spc/nurture.png"
   },
   {
+    id: "spc_scale",
     label: 'Scale Your Mountain',
     description: "We explore with intention and ask hard questions of each other, to find work that matters to us.",
     image: "/spc/scale.png"
   },
   {
+    id: "spc_dig",
     label: 'Dig Deep Wells',
     description: "By showing up authentically and investing in meaningful relationships, we forge bonds that will last beyond our time at SPC.",
     image: "/spc/dig.png"
   },
   {
+    id: "spc_own",
     label: 'Own Your Better Future',
     description: "By immersing yourself and taking initiative, you leave a legacy for those who will walk in your steps. SPC is yours now: you don't need to ask for permission!",
     image: "/spc/own.png"
@@ -35,6 +41,14 @@ const GiveCategory: NextPage = () => {
   const { id } = router.query;
   const [category, setCategory] = useState<string>();
 
+  async function onNext() {
+    await supabase
+        .from<definitions["snaps"]>("snaps")
+        .update({ category })
+        .eq('id', id as string);
+    router.push(`/give/${id}/note`);
+  }
+
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
       <div className="w-96 flex flex-col">
@@ -45,10 +59,10 @@ const GiveCategory: NextPage = () => {
 
         <h2 className="my-2">Select a category:</h2>
 
-        {types.map(t => <Card
+        {spcTypes.map(t => <Card
           key={t.label}
-          selected={category === t.label}
-          onClick={() => setCategory(t.label)}
+          selected={category === t.id}
+          onClick={() => setCategory(t.id)}
           imageUrl={t.image}
           label={t.label}
           description={t.description}
@@ -61,7 +75,7 @@ const GiveCategory: NextPage = () => {
           />
           <PrimaryButton
             text="Next"
-            onClick={() => router.push(`/give/${id}/note`)}
+            onClick={onNext}
             disabled={!category}
           />
         </ButtonContainer>
